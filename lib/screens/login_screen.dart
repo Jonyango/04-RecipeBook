925595b23e85b4982456586e33bc23f1f25ad7bc
Muhammad Talha Bin Mansoor <m.talhabinmansoor@gmail.com>
2020-08-15 09:31:19 +0500

    update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,27 +17,93 @@ class LoginScreen extends StatelessWidget {
           ),
           padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 22.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Welcome to',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Text(
-                        'Recipe Book',
-                        style: TextStyle(fontSize: 36.0),
-                      ),
-                    ],
+                  RichText(
+                    text: TextSpan(
+                      children: <InlineSpan>[
+                        TextSpan(
+                          text: 'Welcome to\n',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        TextSpan(
+                          text: 'Recipe Book',
+                          style: TextStyle(
+                            fontSize: 36.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  Image.asset('assets/images/Logo.png'),
                 ],
               ),
+              CustomTextFormField(
+                placeholder: 'Email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              CustomTextFormField(
+                placeholder: 'Password',
+                obscureText: true,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.white,
+                textColor: Color(0xff34A853),
+                child: Text('Login'),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  CustomTextFormField({
+    this.padding,
+    this.placeholder,
+    this.contentPadding,
+    this.keyboardType,
+    this.obscureText,
+    this.focusNode,
+    this.autoFocus,
+  });
+
+  final EdgeInsets padding;
+  final String placeholder;
+  final EdgeInsets contentPadding;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final FocusNode focusNode;
+  final bool autoFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: this.padding ?? EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        autofocus: autoFocus ?? false,
+        decoration: InputDecoration(
+          labelText: this.placeholder ?? '',
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          fillColor: Colors.white30,
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          contentPadding: this.contentPadding ?? EdgeInsets.all(16.0),
+        ),
+        focusNode: this.focusNode,
+        keyboardType: this.keyboardType,
+        obscureText: this.obscureText ?? false,
       ),
     );
   }
